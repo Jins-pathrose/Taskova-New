45e9bcc9d932b395217df00eb0974dccbda04ac0
Jins-pathrose <jinspathrose560@gmail.com>
2025-05-28 10:39:28 +0530

    28/05/05

--- lib/View/BottomNavigation/bottomnavigation.dart
+++ lib/View/BottomNavigation/bottomnavigation.dart
@@ -1,13 +1,125 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:provider/provider.dart';
+// import 'package:taskova_new/Model/Notifications/notification_service.dart';
+// import 'package:taskova_new/View/ChatPage/chatpage.dart';
+// import 'package:taskova_new/View/Community/community_page.dart';
+// import 'package:taskova_new/View/Homepage/admin_approval.dart';
+// import 'package:taskova_new/View/Homepage/homepage.dart';
+// import 'package:taskova_new/View/Language/language_provider.dart';
+// import 'package:taskova_new/View/Profile/profilepage.dart';
+
+
+// class MainWrapper extends StatefulWidget {
+//   const MainWrapper({Key? key}) : super(key: key);
+
+//   @override
+//   State<MainWrapper> createState() => _MainWrapperState();
+// }
+
+// class _MainWrapperState extends State<MainWrapper> with WidgetsBindingObserver {
+//   final NotificationService _notificationService = NotificationService();
+
+//   int _currentIndex = 0;
+//   late AppLanguage appLanguage;
+
+//   final List<Widget> _pages = [
+//     const HomePage(),
+//     const CommunityPage(),
+//      ProfilePage(),
+
+//   ];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     appLanguage = Provider.of<AppLanguage>(context, listen: false);
+//     WidgetsBinding.instance.addObserver(this);
+    
+//     // Start notification service when user enters the app
+//     _notificationService.startNotificationService();
+//   }
+//     @override
+//   void dispose() {
+//     WidgetsBinding.instance.removeObserver(this);
+    
+//     // Stop notification service when leaving the app
+//     // _notificationService.stopNotificationService();
+//     super.dispose();
+//   }
+  
+//   @override
+//   void didChangeAppLifecycleState(AppLifecycleState state) {
+//     switch (state) {
+//       case AppLifecycleState.resumed:
+//         // App came to foreground, start notifications
+//         _notificationService.startNotificationService();
+//         break;
+//       case AppLifecycleState.paused:
+//       case AppLifecycleState.inactive:
+//         // App went to background, stop notifications
+//         // _notificationService.stopNotificationService();
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoPageScaffold(
+//       backgroundColor: CupertinoColors.systemBackground,
+//       child: CupertinoTabScaffold(
+//         tabBar: CupertinoTabBar(
+//           currentIndex: _currentIndex,
+//           onTap: (index) {
+//             setState(() {
+//               _currentIndex = index;
+//             });
+//           },
+//           backgroundColor: CupertinoColors.systemBackground,
+//           activeColor: CupertinoColors.systemBlue,
+//           inactiveColor: CupertinoColors.systemGrey,
+//           items: [
+//             BottomNavigationBarItem(
+//               icon: Icon(_currentIndex == 0 
+//                   ? CupertinoIcons.house_fill 
+//                   : CupertinoIcons.house),
+//               label: appLanguage.get('Home'),
+//             ),
+            
+//             BottomNavigationBarItem(
+//               icon: Icon(_currentIndex == 1 
+//                   ? CupertinoIcons.person_2_fill 
+//                   : CupertinoIcons.person_2),
+//               label: appLanguage.get('Community'),
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(_currentIndex == 2 
+//                   ? CupertinoIcons.person_fill 
+//                   : CupertinoIcons.person),
+//               label: appLanguage.get('Profile'),
+//             ),
+//           ],
+//         ),
+//         tabBuilder: (context, index) {
+//           return CupertinoTabView(
+//             builder: (context) {
+//               return _pages[index];
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:taskova_new/Model/Notifications/notification_service.dart';
-import 'package:taskova_new/View/ChatPage/chatpage.dart';
 import 'package:taskova_new/View/Community/community_page.dart';
-import 'package:taskova_new/View/Homepage/admin_approval.dart';
 import 'package:taskova_new/View/Homepage/homepage.dart';
 import 'package:taskova_new/View/Language/language_provider.dart';
 import 'package:taskova_new/View/Profile/profilepage.dart';
-
 
 class MainWrapper extends StatefulWidget {
   const MainWrapper({Key? key}) : super(key: key);
@@ -25,8 +137,7 @@ class _MainWrapperState extends State<MainWrapper> with WidgetsBindingObserver {
   final List<Widget> _pages = [
     const HomePage(),
     const CommunityPage(),
-     ProfilePage(),
-
+    const ProfilePage(),
   ];
 
   @override
@@ -34,30 +145,23 @@ class _MainWrapperState extends State<MainWrapper> with WidgetsBindingObserver {
     super.initState();
     appLanguage = Provider.of<AppLanguage>(context, listen: false);
     WidgetsBinding.instance.addObserver(this);
-    
-    // Start notification service when user enters the app
     _notificationService.startNotificationService();
   }
-    @override
+
+  @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
-    
-    // Stop notification service when leaving the app
-    // _notificationService.stopNotificationService();
     super.dispose();
   }
-  
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     switch (state) {
       case AppLifecycleState.resumed:
-        // App came to foreground, start notifications
         _notificationService.startNotificationService();
         break;
       case AppLifecycleState.paused:
       case AppLifecycleState.inactive:
-        // App went to background, stop notifications
-        // _notificationService.stopNotificationService();
         break;
       default:
         break;
@@ -67,6 +171,8 @@ class _MainWrapperState extends State<MainWrapper> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      // Prevent the scaffold from resizing when the keyboard appears
+      resizeToAvoidBottomInset: false,
       backgroundColor: CupertinoColors.systemBackground,
       child: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
@@ -81,21 +187,20 @@ class _MainWrapperState extends State<MainWrapper> with WidgetsBindingObserver {
           inactiveColor: CupertinoColors.systemGrey,
           items: [
             BottomNavigationBarItem(
-              icon: Icon(_currentIndex == 0 
-                  ? CupertinoIcons.house_fill 
+              icon: Icon(_currentIndex == 0
+                  ? CupertinoIcons.house_fill
                   : CupertinoIcons.house),
               label: appLanguage.get('Home'),
             ),
-            
             BottomNavigationBarItem(
-              icon: Icon(_currentIndex == 1 
-                  ? CupertinoIcons.person_2_fill 
+              icon: Icon(_currentIndex == 1
+                  ? CupertinoIcons.person_2_fill
                   : CupertinoIcons.person_2),
               label: appLanguage.get('Community'),
             ),
             BottomNavigationBarItem(
-              icon: Icon(_currentIndex == 2 
-                  ? CupertinoIcons.person_fill 
+              icon: Icon(_currentIndex == 2
+                  ? CupertinoIcons.person_fill
                   : CupertinoIcons.person),
               label: appLanguage.get('Profile'),
             ),
